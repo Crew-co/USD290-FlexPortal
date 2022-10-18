04631204a12f6e87686713c19f0bcbfffee12c21
XST <xstudios383@gmail.com>
2022-10-18 14:37:48 +0000

    updated

--- Test_Site/public/cgi-bin/form.py
+++ Test_Site/public/cgi-bin/form.py
@@ -18,17 +18,20 @@ def GETSTUDENT(stu):
         res = rq.get(
             "https://flex.usd290.org/cyclonehour/admajax.php?search=" + student,verify=False,
         )
-        NL = re.split("=|'>|</a>|<a href|Suggestions</div>\n<div class|</div>\n<div class|<div class|'suggest_link|'suggestions'|</div>\nError, invalid action|'adminStuProf.|php|uid|\?|''",res.text)
+        NL = re.split("=|'>|</a>|<a href|Suggestions</div>\n<div class|</div>\n<div class|<div class|'suggest_link|'suggestions'|</div>\nError, invalid action|'adminStuProf.|php|uid|\?|style|'color: #08c;|No suggestions|''",res.text)
         NL_EMPTY_STRINGS = []
         for string in NL:
             if string != "":
                 NL_EMPTY_STRINGS.append(string)
+                if ' ' in NL_EMPTY_STRINGS:
+                    NL_EMPTY_STRINGS.remove(' ')
+                    print("Person Not Found")
         for LST in NL_EMPTY_STRINGS:
             #print(LST)
             print("<ul>")
             print(f"<li>{LST}</li>")
             print("</ul>")
-        print("<a href=/index.html>Back</a>")
+        print("<a href=/>Back</a>")
     except TypeError as e:
         print("No Name was Given")
         print("<a href=/>Back</a>")
