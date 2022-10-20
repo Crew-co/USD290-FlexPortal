8391b32d78f012472b2d0e95d30a8478bee06d6c
XST <xstudios383@gmail.com>
2022-10-20 01:42:22 +0000

    Small_Update 01

--- Test_Site/public/cgi-bin/student_form.py
+++ Test_Site/public/cgi-bin/student_form.py
@@ -13,6 +13,7 @@ cgitb.enable()  # for debugging
 form = cgi.FieldStorage()
 name = form.getvalue("fname")
 staff = form.getvalue("staff")
+staffID = "ABC"
 
 def GETSTUDENT(stu):
     try:
@@ -38,45 +39,46 @@ def GETSTUDENT(stu):
         print("No Name was Given")
         print("<a href=/>Back</a>")
 def GETSTUDENT_EVENT(stu):
-    try:
-        # student = input("User: ")
-        student = stu
-        res = rq.get(
-            "https://flex.usd290.org/cyclonehour/admajax.php?search=" + student,verify=False,
-        )
-        NL = re.split("=|'>|</a>|<a href|Suggestions</div>\n<div class|</div>\n<div class|<div class|'suggest_link|'suggestions'|</div>\nError, invalid action|'adminStuProf.|php|uid|\?|style|'color: #08c;|No suggestions|''",res.text)
-        NL_EMPTY_STRINGS = []
-        for string in NL:
-            if string != "":
-                NL_EMPTY_STRINGS.append(string)
-                if ' ' in NL_EMPTY_STRINGS:
-                    NL_EMPTY_STRINGS.remove(' ')
-                    #print("Person Not Found")
-        #print(NL_EMPTY_STRINGS)
-        out = NL_EMPTY_STRINGS.pop(0)
-        STID = out
-        start = "2022-10-10T00:00:00-05:00"
-        end = "2022-11-15T00:00:00-05:00"
-        data = {
-            "studentID":STID,
-            "start":start,
-            "end":end
-        }
-        res = rq.post("https://flex.usd290.org/cyclonehour/eventFeed.php",data=data,verify=False)
-        todos = json.loads(res.text)
-        #print("<ul>")
-        print("<div>|=ID|==============Title=================|==============StaffID==============|======Start======|======end========|</div>")
-        for entry in todos:
-            print( "<p>"+"|"+entry['id']+" "+entry['title']+" "+entry['staffid']+" "+entry['start']+" "+entry['end']+"</p>")
-        print("<div>|========================================================================================================|</div>")
+    if staffID == "ABC":
+        try:
+            # student = input("User: ")
+            student = stu
+            res = rq.get("https://flex.usd290.org/cyclonehour/admajax.php?search=" + student,verify=False,)
+            NL = re.split("=|'>|</a>|<a href|Suggestions</div>\n<div class|</div>\n<div class|<div class|'suggest_link|'suggestions'|</div>\nError, invalid action|'adminStuProf.|php|uid|\?|style|'color: #08c;|No suggestions|''",res.text)
+            NL_EMPTY_STRINGS = []
+            for string in NL:
+                if string != "":
+                    NL_EMPTY_STRINGS.append(string)
+                    if ' ' in NL_EMPTY_STRINGS:
+                        NL_EMPTY_STRINGS.remove(' ')
+                        #print("Person Not Found")
+            #print(NL_EMPTY_STRINGS)
+            out = NL_EMPTY_STRINGS.pop(0)
+            STID = out
+            start = "2022-10-10T00:00:00-05:00"
+            end = "2022-11-15T00:00:00-05:00"
+            data = {
+                "studentID":STID,
+                "start":start,
+                "end":end
+            }
+            res = rq.post("https://flex.usd290.org/cyclonehour/eventFeed.php",data=data,verify=False)
+            todos = json.loads(res.text)
+            #print("<ul>")
+            print("<div>|=ID|==============Title=================|==============StaffID==============|======Start======|======end========|</div>")
+            for entry in todos:
+                print( "<p>"+"|"+entry['id']+" "+entry['title']+" "+entry['staffid']+" "+entry['start']+" "+entry['end']+"</p>")
+            print("<div>|========================================================================================================|</div>")
         #print("</ul>")
-        print("<a href=/>Back</a>")
-    except TypeError as e:
-        print("No Name was Given")
-        print("<a href=/>Back</a>")
-    except IndexError as e:
-        print("User Events Not found")
-        print("<a href=/>Back</a>")
+            print("<a href=/>Back</a>")
+        except TypeError as e:
+            print("No Name was Given")
+            print("<a href=/>Back</a>")
+        except IndexError as e:
+            print("User Events Not found")
+            print("<a href=/>Back</a>")
+    else:
+        print("FAIL")
 def Get_Events_STAFF(SD):
     # staffID = input("ID: ")
     staffID = SD
